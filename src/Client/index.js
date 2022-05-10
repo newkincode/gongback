a315df8fec6e575a2f26e3b2fbbf7afcd56a4cc5
newkincode <94734506+newkincode@users.noreply.github.com>
2022-05-10 22:11:53 +0900

    Initial commit

---
+++ src/Client/index.js
@@ -0,0 +1,69 @@
+import { Client, Collection, Intents } from 'discord.js'
+import { readdirSync } from 'fs'
+import path from 'path'
+import { config } from 'dotenv'
+
+export class Command {
+  constructor() {
+    this.name = ''
+    this.description = ''
+    this.defaultPermission = undefined
+    this.type = 'CHAT_INPUT'
+    this.options = []
+  }
+
+  execute(interaction) {}
+}
+
+export class mbprClient extends Client {
+  constructor() {
+    super({
+      intents: [Intents.FLAGS.GUILDS],
+    })
+    this._commands = new Collection()
+    this._commandDirectory = path.join(__dirname, '..', 'Commands')
+  }
+
+  /**
+   * @private
+   */
+  _loadCommands() {
+    const Directory = readdirSync(path.join(this._commandDirectory))
+    for (const Folder of Directory) {
+      const Dir2 = readdirSync(`${this._commandDirectory}/${Folder}`)
+      for (const File of Dir2) {
+        const Temp = require(`${this._commandDirectory}/${Folder}/${File}`)
+        const modules = new Temp()
+        this._commands.set(modules.name, modules)
+        this.once('ready', () => {
+          this.application?.commands.create({
+            name: modules.name,
+            description: modules.description,
+            type: modules.type,
+            options: modules.options,
+            defaultPermission: modules.defaultPermission,
+          })
+        })
+      }
+    }
+  }
+
+  start() {
+    config()
+    this.login(process.env.TOKEN)
+    this.on('ready', () => {
+      console.log(`[Client] ${this.user.username}`)
+      console.log('-------------------------')
+    })
+    this._loadCommands()
+    this.on('interactionCreate', interaction => {
+      if (interaction.isCommand()) {
+        const Command = this._commands.get(interaction.commandName)
+
+        if (!Command) return
+
+        Command.execute(interaction)
+      }
+    })
+  }
+}
---
+++ src/Commands/info/help.js
@@ -0,0 +1,31 @@
+import { Command } from '../../Client'
+import { Formatters, MessageEmbed } from 'discord.js'
+
+module.exports = class extends Command {
+  constructor() {
+    super()
+    this.name = 'help'
+    this.description = "mbpr project's Help"
+  }
+
+  execute(interaction) {
+    interaction.reply({
+      embeds: [
+        new MessageEmbed()
+          .setTitle(`${interaction.client.user.username}'s Help`)
+          .setDescription(
+            Formatters.codeBlock(
+              'md',
+              `# info
+- help
+
+# mod
+- kick
+- ban
+- clean`
+            )
+          ),
+      ],
+    })
+  }
+}
